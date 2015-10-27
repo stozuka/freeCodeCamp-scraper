d83664b94d20025e59acd7e5eb47c2bc512a1097
stozuka <stozuka@gmail.com>
2015-10-27 21:26:08 +0800

    just made the codes look better

--- spec/fcp_spec.rb
+++ spec/fcp_spec.rb
@@ -12,26 +12,14 @@ end
 VCR.use_cassette('completed_information') do
   describe 'test' do
 
-    # VCR.insert_cassette('stozuka')
     obj = SiteScraper.new('stozuka')
-    obj.file_output # create a test output file
-
-    # before do
-    #   VCR.insert_cassette('stozuka')
-    #   obj = SiteScraper.new(name)
-    #   obj.file_output # create a test output file
-    # end
+    obj.file_output
 
     it 'has same course names and dates' do
       `diff output_spec.txt ./spec/fixtures/output.txt`.must_equal ''
     end
 
     `rm output_spec.txt`
-    # VCR.eject_cassette
 
-    # after do
-    #   `rm output_spec.txt`
-    #   VCR.eject_cassette
-    # end
   end
 end
