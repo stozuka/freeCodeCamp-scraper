7bc9f53f4cfb6a87f6f964c2199a9e3097c3ec41
stozuka <stozuka@gmail.com>
2015-10-27 20:16:21 +0800

    change in fcp_spec.rb and .trafvis.yml

--- spec/fcp_spec.rb
+++ spec/fcp_spec.rb
@@ -11,19 +11,27 @@ end
 
 VCR.use_cassette('completed_information') do
   describe 'test' do
-    before do
-      VCR.insert_cassette('stozuka')
-      obj = SiteScraper.new('stozuka')
-      obj.file_output # create a test output file
-    end
+
+    # VCR.insert_cassette('stozuka')
+    obj = SiteScraper.new('stozuka')
+    obj.file_output # create a test output file
+
+    # before do
+    #   VCR.insert_cassette('stozuka')
+    #   obj = SiteScraper.new(name)
+    #   obj.file_output # create a test output file
+    # end
 
     it 'has same course names and dates' do
       `diff output_spec.txt ./spec/fixtures/output.txt`.must_equal ''
     end
 
-    after do
-      `rm output_spec.txt`
-      VCR.eject_cassette
-    end
+    `rm output_spec.txt`
+    # VCR.eject_cassette
+
+    # after do
+    #   `rm output_spec.txt`
+    #   VCR.eject_cassette
+    # end
   end
 end
