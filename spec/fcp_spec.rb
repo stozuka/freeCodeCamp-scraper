60c3474b3ef84e0eb92925fd0775bc87167860a7
stozuka <stozuka@gmail.com>
2015-10-27 19:54:18 +0800

    deleted spec in .gitignore

---
+++ spec/fcp_spec.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'vcr'
+require 'webmock/minitest'
+require_relative '../lib/fcp_class'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
+
+VCR.use_cassette('completed_information') do
+  describe 'test' do
+    before do
+      VCR.insert_cassette('stozuka')
+      obj = SiteScraper.new('stozuka')
+      obj.file_output # create a test output file
+    end
+
+    it 'has same course names and dates' do
+      `diff output_spec.txt ./spec/fixtures/output.txt`.must_equal ''
+    end
+
+    after do
+      `rm output_spec.txt`
+      VCR.eject_cassette
+    end
+  end
+end
